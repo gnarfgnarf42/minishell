8ed701443af354aa75fe2d6e530feb3acb64f05a
nefimov <n.a.efimov@gmail.com>
2025-04-28 09:12:09 +0200

    define SH_PATH /bin/sh

--- includes/execution.h
+++ includes/execution.h
@@ -6,12 +6,14 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 17:21:49 by nefimov           #+#    #+#             */
-/*   Updated: 2025/04/22 17:46:37 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/04/25 14:11:19 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTION_H
 # define EXECUTION_H
+
+# define SH_PATH "/bin/sh" 
 
 # include <sys/wait.h>
 # include "parser.h"
@@ -19,6 +21,7 @@
 typedef struct s_command
 {
 	char 	*pathname;
+	// char	*path;
 	char 	**args;
 	char	**envp;
 	int		fd_in;
@@ -26,6 +29,6 @@ typedef struct s_command
 	int		exit_val;
 }	t_command;
 
-void ft_exec_command(t_command *cmd);
+void ft_exec_command(t_shell *shell, t_command *cmd);
 
 #endif
