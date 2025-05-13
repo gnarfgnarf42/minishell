a16d9f299de64eb09f261da3b5a4ff5f6d3e6661
nefimov <n.a.efimov@gmail.com>
2025-05-13 16:10:57 +0200

    add pipe fd's creation and redirection before execution

--- src/execution/exec/ft_exec_command.c
+++ src/execution/exec/ft_exec_command.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 13:06:17 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/12 09:43:07 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/13 15:58:46 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,31 @@
 #include <errno.h>
 
 static int	get_exit_code(pid_t pid);
+
+static int	dup_fd(t_command *cmd)
+{	
+	if (cmd->next && cmd->fd_pipe[1] != STDOUT_FILENO)
+	{
+		dup2(cmd->fd_pipe[1], STDOUT_FILENO);
+		close(cmd->fd_pipe[1]);		
+	}
+	if (cmd->prev && cmd->prev->fd_pipe[0] != STDIN_FILENO)
+	{
+		dup2(cmd->prev->fd_pipe[0], STDIN_FILENO);
+		close(cmd->prev->fd_pipe[0]);
+	}
+	if (cmd->fd_in != STDIN_FILENO)
+	{
+		dup2(cmd->fd_in, STDIN_FILENO);
+		close(cmd->fd_in);
+	}
+	if (cmd->fd_out != STDOUT_FILENO)
+	{
+		dup2(cmd->fd_out, STDOUT_FILENO);
+		close(cmd->fd_out);
+	}
+	return (0);
+}
 
 void ft_exec_command(t_shell *shell, t_command *cmd)
 {
@@ -30,6 +55,7 @@ void ft_exec_command(t_shell *shell, t_command *cmd)
 	pid = fork();
 	if (pid == 0)
 	{
+		dup_fd(cmd);
 		execve(cmd->cmdname, cmd->args, cmd->envp);
 		if (errno == ENOEXEC)
 		{
@@ -50,6 +76,10 @@ void ft_exec_command(t_shell *shell, t_command *cmd)
 	}
 	else
 	{
+		if (cmd->next && cmd->fd_pipe[1] != STDOUT_FILENO)
+			close(cmd->fd_pipe[1]);		
+		if (cmd->prev && cmd->prev->fd_pipe[0] != STDIN_FILENO)
+			close(cmd->prev->fd_pipe[0]);
 		cmd->exit_val = get_exit_code(pid);
 		printf("Exit code: %d\n", cmd->exit_val);
 	}
--- src/execution/proc/ft_proc_pipe.c
+++ src/execution/proc/ft_proc_pipe.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 12:02:41 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/12 11:53:11 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/13 15:01:10 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,30 @@ t_token		*ft_process_pipe(t_shell *shell, t_token *token, t_command **cmd)
 	
 	token = token->next;
 	if (token->type == TOKEN_PIPE || token->type == TOKEN_END)
+	{
+		write(STDERR_FILENO, "-minishell: syntax error near unexpected token\n", 47);
+		(*cmd)->exit_val = 2;  
 		return (NULL);
+	}
 	printf("TOKEN_PIPE: %s\n", token->value);
-	new_cmd = ft_init_cmd(shell);
 	// Create new cmd
+	new_cmd = ft_init_cmd(shell);
+	new_cmd->prev = *cmd;
 	(*cmd)->next = new_cmd;
-	// Create pipe, redirect fd's, create new cmd, redirect fd's
+	// Create pipe
+	if (pipe((*cmd)->fd_pipe) == -1)
+	{
+		perror("-minishell");
+		(*cmd)->exit_val = 1;
+		return (NULL);
+	}
+	// if ((*cmd)->prev == NULL)
+	// 	if (close((*cmd)->fd_pipe[0] == -1))
+	// 	{
+	// 		perror("-minishell");
+	// 		(*cmd)->exit_val = 1;
+	// 		return (NULL);
+	// 	}
 	*cmd = new_cmd;
 	return (token);
 }
