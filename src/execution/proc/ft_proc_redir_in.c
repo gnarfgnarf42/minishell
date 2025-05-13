1c255c8588883dd95ad83a6411dea2aab4694977
nefimov <n.a.efimov@gmail.com>
2025-05-13 16:13:41 +0200

    add fd redirections for ft_proc_redir_in()

--- src/execution/proc/ft_proc_redir_in.c
+++ src/execution/proc/ft_proc_redir_in.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 12:02:41 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/12 16:56:08 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/13 16:08:13 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,34 @@ t_token		*ft_process_redir_in(t_shell *shell, t_token *token, t_command *cmd)
 	int	fd;
 	
 	(void)shell;
-	(void)cmd;
 	token = token->next;
+	// Check for syntax errors
 	if (token->type != TOKEN_WORD)
 	{
 		// Syntax error
-		// print error msg
-		// set exit_val in cmd 
+		write(STDERR_FILENO, "-minishell: syntax error near unexpected token\n", 47);
+		cmd->exit_val = 2;  
 		return (NULL);
 	}
 	printf("TOKEN_REDIR_IN: %s\n", token->value);
-	// Open file for read and write fd
+	// Open file for read
 	fd = open(token->value, O_RDONLY);
 	if (fd == -1)
 	{
 		// File open error
-		// print error msg
-		// set exit_val in cmd 
+		perror("-minishell");
+		cmd->exit_val = 1; 
 		return (NULL);
 	}
+	// Closing previous redirection in
 	if (cmd->fd_in != STDIN_FILENO && close(cmd->fd_in) == -1)
 	{
 		// File close error
-		// print error msg
-		// set exit_val in cmd 
+		perror("-minishell");
+		cmd->exit_val = 1; 
 		return (NULL);
 	}
+	// Change fd_in in cmd
 	cmd->fd_in = fd;
 	return (token->next);
 }
