5231d9fd72584e0082adb061155cd9f294feccd9
nefimov <n.a.efimov@gmail.com>
2025-05-16 14:34:34 +0200

    add args to main function and initialisation of shell.envp

--- src/init/main.c
+++ src/init/main.c
@@ -3,22 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sscholz <sscholz@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 19:22:34 by sscholz           #+#    #+#             */
-/*   Updated: 2025/03/17 19:22:37 by sscholz          ###   ########.fr       */
+/*   Updated: 2025/05/16 14:33:18 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parser.h"
 
-int	main(void)
+int	main(int argc, char *argv[], char *envp[])
 {
 	t_shell	shell;
 
+	(void)argc;
+	(void)argv;
 	shell.tokens = NULL;
 	shell.memory_list = NULL;
+	shell.envp = ft_envp_cpy(&shell, envp);
 	ft_minishell_loop(&shell);
 	return (0);
 }
