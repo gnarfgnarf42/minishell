54abe07a223968270f91e8ee034e5524e27652a5
nefimov <n.a.efimov@gmail.com>
2025-05-16 17:17:30 +0200

    ft_ms_unset(): add implementation

--- src/execution/cmd/ft_ms_unset.c
+++ src/execution/cmd/ft_ms_unset.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:16:57 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/15 15:08:29 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/16 17:14:50 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,42 @@
 
 int	ft_ms_unset(t_shell *shell, t_command *cmd)
 {
-	(void)shell;
-	(void)cmd;
+	char	**arg;
+	char	**env;
+	char	*tmp;
+	int		out;
 	
-	if (write(STDOUT_FILENO, "unset\n", 6) == -1)
+	if (shell == NULL || cmd == NULL)
+		return (2);
+	out = 0;
+	// for each arg
+	arg = cmd->args;
+	while (*(++arg))
 	{
-		perror("-minishell");
-		cmd->exit_val = 1;
-		return (-1);
+		// Try to find name in env
+		env = shell->envp;
+		while (*env)
+		{
+			// If name is in env, remove the line
+			if (ft_strncmp(*env, *arg, ft_strlen(*arg)) == 0 && (*env)[ft_strlen(*arg)] == '=')
+			{
+				tmp = *env;
+				while (*env)
+				{
+					*env = *(env + 1);
+					env = env + 1;
+				}
+				ft_track_free(shell, tmp);
+				break;
+			}
+			env++;
+		}
 	}
-	return (0);
+	// if (write(STDOUT_FILENO, "export\n", 7) == -1)
+	// {
+	// 	perror("-minishell");
+	// 	cmd->exit_val = 1;
+	// 	return (-1);
+	// }
+	return (out);
 }
