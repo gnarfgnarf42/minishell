1b37fc64faf884a79dfa67f25086add7cbbd18a1
nefimov <n.a.efimov@gmail.com>
2025-05-16 14:35:52 +0200

    ft_ms_env(): add implementation

--- src/execution/cmd/ft_ms_env.c
+++ src/execution/cmd/ft_ms_env.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:16:57 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/15 15:08:09 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/16 14:34:57 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,8 @@
 
 int	ft_ms_env(t_shell *shell, t_command *cmd)
 {
-	(void)shell;
 	(void)cmd;
 	
-	if (write(STDOUT_FILENO, "env\n", 4) == -1)
-	{
-		perror("-minishell");
-		cmd->exit_val = 1;
-		return (-1);
-	}
+	ft_penv(shell);
 	return (0);
 }
