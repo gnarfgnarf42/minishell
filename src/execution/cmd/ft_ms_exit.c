9a37bddf17f39382a886f94f85fc0c5a8a44e1af
nefimov <n.a.efimov@gmail.com>
2025-05-19 12:27:02 +0200

    ft_ms_exit(): remove write and change exit arg to 0

--- src/execution/cmd/ft_ms_exit.c
+++ src/execution/cmd/ft_ms_exit.c
@@ -6,7 +6,7 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:16:57 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/18 16:00:43 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/05/19 12:02:44 by nefimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ int	ft_ms_exit(t_shell *shell, t_command *cmd)
 {
 	(void)shell;
 	(void)cmd;
-	if (write(STDOUT_FILENO, "exit\n", 5) == -1)
-	{
-		perror("-minishell");
-		cmd->exit_val = 1;
-		return (-1);
-	}
+	// if (write(STDOUT_FILENO, "exit\n", 5) == -1)
+	// {
+	// 	perror("-minishell");
+	// 	cmd->exit_val = 1;
+	// 	exit(0);
+	// }
 
 	// ft_free_cmd_line(shell, cmd);
 	ft_free_all_tracked(shell);
-	exit(1);
+	exit(0);
 }
