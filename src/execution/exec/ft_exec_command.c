fc94b1c60ef31b789322a16b4ba3d1e43510a902
nefimov <nefimov@student.42berlin.de>
2025-06-28 00:49:58 +0200

    Remake execution pipeline to work with big pipe input. Unstructured code

--- src/execution/exec/ft_exec_command.c
+++ src/execution/exec/ft_exec_command.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_exec_command.c                                  :+:      :+:    :+:   */
@@ -6,120 +6,120 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 13:06:17 by nefimov           #+#    #+#             */
-/*   Updated: 2025/06/25 16:45:18 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/06/27 23:50:30 by nefimov          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "execution.h"
 #include <errno.h>
 #include <signal.h>
 
-static int	get_exit_code(pid_t pid);
+// static int	get_exit_code(pid_t pid);
 
-static int	dup_fd(t_command *cmd)
-{
-	if (cmd->next && cmd->fd_pipe[1] != STDOUT_FILENO)
-	{
-		dup2(cmd->fd_pipe[1], STDOUT_FILENO);
-		close(cmd->fd_pipe[1]);
-	}
-	if (cmd->prev && cmd->prev->fd_pipe[0] != STDIN_FILENO)
-	{
-		dup2(cmd->prev->fd_pipe[0], STDIN_FILENO);
-		close(cmd->prev->fd_pipe[0]);
-	}
-	if (cmd->fd_in != STDIN_FILENO)
-	{
-		dup2(cmd->fd_in, STDIN_FILENO);
-		close(cmd->fd_in);
-	}
-	if (cmd->fd_out != STDOUT_FILENO)
-	{
-		dup2(cmd->fd_out, STDOUT_FILENO);
-		close(cmd->fd_out);
-	}
-	return (0);
-}
+// static int	dup_fd(t_command *cmd)
+// {
+// 	if (cmd->next && cmd->fd_pipe[1] != STDOUT_FILENO)
+// 	{
+// 		dup2(cmd->fd_pipe[1], STDOUT_FILENO);
+// 		close(cmd->fd_pipe[1]);
+// 	}
+// 	if (cmd->prev && cmd->prev->fd_pipe[0] != STDIN_FILENO)
+// 	{
+// 		dup2(cmd->prev->fd_pipe[0], STDIN_FILENO);
+// 		close(cmd->prev->fd_pipe[0]);
+// 	}
+// 	if (cmd->fd_in != STDIN_FILENO)
+// 	{
+// 		dup2(cmd->fd_in, STDIN_FILENO);
+// 		close(cmd->fd_in);
+// 	}
+// 	if (cmd->fd_out != STDOUT_FILENO)
+// 	{
+// 		dup2(cmd->fd_out, STDOUT_FILENO);
+// 		close(cmd->fd_out);
+// 	}
+// 	return (0);
+// }
 
-void	ft_exec_command(t_shell *shell, t_command *cmd)
-{
-	pid_t	pid;
-	int		i;
-	int		old_fd[2];
+// void	ft_exec_command(t_shell *shell, t_command *cmd)
+// {
+// 	pid_t	pid;
+// 	int		i;
+// 	int		old_fd[2];
 
-	if (!ft_cmd_is_builtin(shell, cmd) && ft_get_path(shell, cmd) != 0)
-	{
-		cmd->exit_val = 127;
-		perror("Path error");
-		return ;
-	}
-	if (cmd->is_builtin)
-	{
-		old_fd[0] = dup(STDIN_FILENO);
-		old_fd[1] = dup(STDOUT_FILENO);
-		dup_fd(cmd);
-		cmd->exit_val = ft_exec_builtin(shell, cmd);
-		dup2(old_fd[0], STDIN_FILENO);
-		dup2(old_fd[1], STDOUT_FILENO);
-		close(old_fd[0]);
-		close(old_fd[1]);
-	}
-	else
-	{
-		pid = fork();
-		if (pid == 0)
-		{
-			signal(SIGQUIT, SIG_DFL); // Restore default handler
-			signal(SIGINT, SIG_DFL);
-			dup_fd(cmd);
-			// {
-			execve(cmd->cmdname, cmd->args, cmd->envp);
-			if (errno == ENOEXEC)
-			{
-				i = 0;
-				while (cmd->args[i])
-					i++;
-				while (i >= 0)
-				{
-					cmd->args[i + 1] = cmd->args[i];
-					i--;
-				}
-				cmd->args[0] = SH_PATH;
-				execve(SH_PATH, cmd->args, cmd->envp);
-			}
-			perror("execve failed");
-			cmd->exit_val = 127;
-			exit(127);
-			// }
-		}
-		else
-			cmd->exit_val = get_exit_code(pid);
-	}
-}
+// 	if (!ft_cmd_is_builtin(shell, cmd) && ft_get_path(shell, cmd) != 0)
+// 	{
+// 		cmd->exit_val = 127;
+// 		perror("Path error");
+// 		return ;
+// 	}
+// 	if (cmd->is_builtin)
+// 	{
+// 		old_fd[0] = dup(STDIN_FILENO);
+// 		old_fd[1] = dup(STDOUT_FILENO);
+// 		dup_fd(cmd);
+// 		cmd->exit_val = ft_exec_builtin(shell, cmd);
+// 		dup2(old_fd[0], STDIN_FILENO);
+// 		dup2(old_fd[1], STDOUT_FILENO);
+// 		close(old_fd[0]);
+// 		close(old_fd[1]);
+// 	}
+// 	else
+// 	{
+// 		pid = fork();
+// 		if (pid == 0)
+// 		{
+// 			signal(SIGQUIT, SIG_DFL); // Restore default handler
+// 			signal(SIGINT, SIG_DFL);
+// 			dup_fd(cmd);
+// 			// {
+// 			execve(cmd->cmdname, cmd->args, cmd->envp);
+// 			if (errno == ENOEXEC)
+// 			{
+// 				i = 0;
+// 				while (cmd->args[i])
+// 					i++;
+// 				while (i >= 0)
+// 				{
+// 					cmd->args[i + 1] = cmd->args[i];
+// 					i--;
+// 				}
+// 				cmd->args[0] = SH_PATH;
+// 				execve(SH_PATH, cmd->args, cmd->envp);
+// 			}
+// 			perror("execve failed");
+// 			cmd->exit_val = 127;
+// 			exit(127);
+// 			// }
+// 		}
+// 		else
+// 			cmd->exit_val = get_exit_code(pid);
+// 	}
+// }
 
-static int	get_exit_code(pid_t pid)
-{
-	int	status;
+// static int	get_exit_code(pid_t pid)
+// {
+// 	int	status;
 
-	if (pid < 0)
-	{
-		perror("fork failed");
-		return (255);
-	}
-	if (waitpid(pid, &status, 0) == -1)
-	{
-		perror("waitpid failed");
-		ft_putchar_fd('\n', STDOUT_FILENO);
-		return (255);
-	}
-	if (WIFEXITED(status))
-	{
-		return (WEXITSTATUS(status));
-	}
-	else if (WIFSIGNALED(status))
-	{
-		ft_putchar_fd('\n', STDOUT_FILENO);
-		return (128 + WTERMSIG(status));
-	}
-	return (255);
-}
+// 	if (pid < 0)
+// 	{
+// 		perror("fork failed");
+// 		return (255);
+// 	}
+// 	if (waitpid(pid, &status, 0) == -1)
+// 	{
+// 		perror("waitpid failed");
+// 		ft_putchar_fd('\n', STDOUT_FILENO);
+// 		return (255);
+// 	}
+// 	if (WIFEXITED(status))
+// 	{
+// 		return (WEXITSTATUS(status));
+// 	}
+// 	else if (WIFSIGNALED(status))
+// 	{
+// 		ft_putchar_fd('\n', STDOUT_FILENO);
+// 		return (128 + WTERMSIG(status));
+// 	}
+// 	return (255);
+// }
--- src/execution/exec/ft_exec_shell.c
+++ src/execution/exec/ft_exec_shell.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_exec_shell.c                                    :+:      :+:    :+:   */
@@ -6,41 +6,185 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 17:37:59 by nefimov           #+#    #+#             */
-/*   Updated: 2025/05/16 17:54:50 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/06/28 00:35:20 by nefimov          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "execution.h"
 #include "minishell.h"
 #include "parser.h"
+#include <errno.h>
+#include <signal.h>
 
-static void	write_exit_code(t_shell *shell, t_command *cmd_line)
+// static void	write_exit_code(t_shell *shell)
+// {
+// 	t_command	*cmd;
+	
+// 	if (!shell)
+// 		return ;
+// 	cmd = shell->cmd_list;
+// 	while (cmd->next)
+// 		cmd = cmd->next;
+// 	shell->last_exit_status = cmd->exit_val;
+// 	return ;
+// }
+
+static void	dup_fd(t_command *cmd)
 {
-	if (!shell || !cmd_line)
-		return ;
-	while (cmd_line->next)
-		cmd_line = cmd_line->next;
-	shell->last_exit_status = cmd_line->exit_val;
-	return ;
+	if (cmd->next && cmd->fd_pipe[1] != STDOUT_FILENO)
+		dup2(cmd->fd_pipe[1], STDOUT_FILENO);
+	if (cmd->prev && cmd->prev->fd_pipe[0] != STDIN_FILENO)
+		dup2(cmd->prev->fd_pipe[0], STDIN_FILENO);
+	if (cmd->fd_in != STDIN_FILENO)
+		dup2(cmd->fd_in, STDIN_FILENO);
+	if (cmd->fd_out != STDOUT_FILENO)
+		dup2(cmd->fd_out, STDOUT_FILENO);
+}
+
+static int	close_fd(t_command *cmd)
+{
+	if (cmd->next)
+	{
+		close(cmd->fd_pipe[0]);
+		close(cmd->fd_pipe[1]);
+	}
+	if (cmd->fd_in != STDIN_FILENO)
+		close(cmd->fd_in);
+	if (cmd->fd_out != STDOUT_FILENO)
+		close(cmd->fd_out);
+	return (0);
+}
+
+static int	close_all_fd(t_shell *shell)
+{
+	t_command	*cmd;
+
+	cmd = shell->cmd_list;
+	while (cmd)
+	{
+		close_fd(cmd);
+		cmd = cmd->next;
+	}
+	return (0);
 }
 
 int	ft_exec_shell(t_shell *shell)
 {
 	t_command	*cmd;
-	t_command	*cmd_line;
+	int			i;
+	int			status;
+	int			old_fd[2];
 
-	cmd_line = ft_create_cmd_line(shell);
-	if (!cmd_line)
+	shell->cmd_list = ft_create_cmd_line(shell);
+	if (!shell->cmd_list)
 		return (1);
-	cmd = cmd_line;
+	
+	// Check if cmd is builtin and set cmd->is_builtin
+	cmd = shell->cmd_list;
 	while (cmd)
 	{
-		if (cmd->exit_val == 0)
-			ft_exec_command(shell, cmd);
-		write_exit_code(shell, cmd_line);
-		ft_close_cmd_fd(cmd);
+		ft_cmd_is_builtin(shell, cmd);
 		cmd = cmd->next;
 	}
-	ft_free_cmd_line(shell, cmd_line);
+	// Create fork. Put PID to cmd->pid
+	cmd = shell->cmd_list;
+	while (cmd)
+	{
+		if (cmd->is_builtin)
+		{
+			old_fd[0] = dup(STDIN_FILENO);
+			old_fd[1] = dup(STDOUT_FILENO);
+			dup_fd(cmd);
+			cmd->exit_val = ft_exec_builtin(shell, cmd);
+			dup2(old_fd[0], STDIN_FILENO);
+			dup2(old_fd[1], STDOUT_FILENO);
+			close(old_fd[0]);
+			close(old_fd[1]);
+		}
+		else if (ft_get_path(shell, cmd) != 0)
+		{
+			cmd->exit_val = 127;
+			perror("Path error");
+			return (1);
+		}
+		else
+		{
+			cmd->pid = fork();
+			if (cmd->pid < 0) // Fork error
+			{
+				cmd->exit_val = 255;
+				perror("fork failed");
+				return (1);
+			}
+			if (cmd->pid == 0) // Child process
+			{
+				signal(SIGQUIT, SIG_DFL); // Restore default handler
+				signal(SIGINT, SIG_DFL);
+				dup_fd(cmd);
+				close_all_fd(shell);
+				execve(cmd->cmdname, cmd->args, cmd->envp);
+				if (errno == ENOEXEC)
+				{
+					i = 0;
+					while (cmd->args[i])
+						i++;
+					while (i >= 0)
+					{
+						cmd->args[i + 1] = cmd->args[i];
+						i--;
+					}
+					cmd->args[0] = SH_PATH;
+					execve(SH_PATH, cmd->args, cmd->envp);
+				}
+				perror("execve failed");
+				cmd->exit_val = 127;
+				exit(127);
+			}
+		}
+		cmd = cmd->next;
+	}
+	// Parent closes all pipe fds
+	close_all_fd(shell);
+    // Wait for all children and store exit codes
+	cmd = shell->cmd_list;
+	while (cmd)
+	{
+		if (cmd->pid < 0)
+		{
+			cmd = cmd->next;
+			continue;
+		}
+		if (waitpid(cmd->pid, &status, 0) == -1)
+		{
+			perror("waitpid failed");
+			ft_putchar_fd('\n', STDOUT_FILENO);
+			cmd->exit_val = 255;
+			// return (255);
+		}
+		if (WIFEXITED(status))
+		{
+			cmd->exit_val = status;
+			// return (WEXITSTATUS(status));
+		}
+		else if (WIFSIGNALED(status))
+		{
+			ft_putchar_fd('\n', STDOUT_FILENO);
+			cmd->exit_val = 128 + WTERMSIG(status);
+			// return (128 + WTERMSIG(status));
+		}
+		cmd = cmd->next;
+	}
+
+	// cmd = shell->cmd_list;
+	// while (cmd)
+	// {
+	// 	if (cmd->exit_val == 0)
+	// 		ft_exec_command(shell, cmd);
+	// 	write_exit_code(shell);
+	// 	ft_close_cmd_fd(cmd);
+	// 	cmd = cmd->next;
+	// }
+	
+	ft_free_cmd_line(shell, shell->cmd_list);
 	return (0);
 }
