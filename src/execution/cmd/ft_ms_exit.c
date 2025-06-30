4b8fe73e81fc63224bcfe6016b3efffdb3155c2c
nefimov <nefimov@student.42berlin.de>
2025-06-30 17:49:47 +0200

    Correct ft_ms_exit() function

--- src/execution/cmd/ft_ms_exit.c
+++ src/execution/cmd/ft_ms_exit.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_ms_exit.c                                       :+:      :+:    :+:   */
@@ -6,26 +6,68 @@
 /*   By: nefimov <nefimov@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:16:57 by nefimov           #+#    #+#             */
-/*   Updated: 2025/06/26 16:52:25 by nefimov          ###   ########.fr       */
+/*   Updated: 2025/06/30 17:31:45 by nefimov          ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "execution.h"
 #include "minishell.h"
 #include "parser.h"
 
+static int	is_not_numeric(char	*arg)
+{
+	if (!arg)
+		return (1);
+	if (*arg == 0)
+		return (2);
+	if (*arg == '+' || *arg == '-')
+		arg++;
+	while (*arg)
+	{
+		if (*arg < '0' || *arg > '9')
+			return (2);
+		arg++;
+	}
+	return (0);
+}
+
+static int	perror_exit(char *arg, char *msg, int code)
+{
+	ft_putstr_fd("-minishell: exit: ", STDERR_FILENO);
+	if (*arg)
+	{
+		ft_putstr_fd("`", STDERR_FILENO);
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putstr_fd("': ", STDERR_FILENO);
+	}
+	ft_putstr_fd(msg, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	return ((unsigned char)code);
+}
+
 int	ft_ms_exit(t_shell *shell, t_command *cmd)
 {
-	(void)shell;
-	(void)cmd;
-	if (write(STDOUT_FILENO, "exit\n", 5) == -1)
-	{
-		perror("-minishell");
-		cmd->exit_val = 1;
-		exit(0);
-	}
+	int code;
 
-	// ft_free_cmd_line(shell, cmd);
-	ft_free_all_tracked(shell);
-	exit(0);
+	
+	if (cmd->argsc == 1)
+    	code = shell->last_exit_status;
+	else if (cmd->argsc > 2)
+		code = perror_exit("", "too many arguments", 1);
+	else if (cmd->argsc == 2)
+	{
+		if (is_not_numeric(cmd->args[1]))
+			code = perror_exit(cmd->args[1], "numeric argument required", 2);
+		else
+			code = ft_atoi(cmd->args[1]);
+	}
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
+		ft_putstr_fd("exit\n", STDOUT_FILENO);
+	// ft_close_all_fd(shell);
+	// ft_free_all_tracked(shell);
+	cmd->exit_val = code & 0xFF;
+	shell->exit = 0;
+	// printf("exit code: %i\n", cmd->exit_val);
+	// exit(cmd->exit_val);
+	return (cmd->exit_val);
 }
